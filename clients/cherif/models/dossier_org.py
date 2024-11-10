55ed9b03c5e5742baa306763b26b1e9627ccc734
soufnet39 <soufnet39@gmail.com>
2024-11-10 23:21:00 +0100

    correction last commit

--- clients/cherif/models/dossier_org.py
+++ clients/cherif/models/dossier_org.py
@@ -3,7 +3,9 @@ from odoo import models, fields
 class NticCherifDossierOrg(models.Model):
     _name = "cherif.dossierorg"
     _description = "Dossier Origine"
+    _order = 'sequence asc'
     
+    sequence = fields.Integer(string='Sequence', default=10 )
     dossier_id = fields.Char('dossier id',required=True)
     name = fields.Char('Name',required=True)
    
